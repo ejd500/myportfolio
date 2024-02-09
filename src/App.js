c22e88671b4eae3fe3a52a4eac47722a8b300033
ejd500 <ellen.dalton-dawe@keyin.com>
2024-02-09 12:33:16 -0330

    added basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Projects from "./components/Projects";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/myportfolio">
       <Routes>
         <Route index element={<Home/>} />
         <Route path="/about" element={<About/>} />
